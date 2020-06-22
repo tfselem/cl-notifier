9f50a0d2890107de2ed7bef57c66c13aadca1f24
Thomas Selem <tfselem@gmail.com>
2020-06-21 21:12:54 -0400

    Added XMLHttpRequest functionality and parsing to DOM

--- scripts/url-validator.js
+++ scripts/url-validator.js
@@ -9,8 +9,19 @@ chrome.tabs.query({
     url: notifier.url,
     status: "complete" 
 }, function(tabs) {
-    let tabID = tabs[0] ? tabs[0].id : null;
-    console.log(tabID);
-    return false;
+    if (tabs[0]) {
+        console.log(tabs[0].url);
+        let url = tabs[0].url,
+            xhr = new XMLHttpRequest();
+        xhr.open("GET", url, true);
+        xhr.responseType = "document";
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == XMLHttpRequest.DONE) {
+                console.log(xhr.response.getElementById("sortable-results").childNodes);
+            }
+        }
+        xhr.send();
+    }
 });
+
 

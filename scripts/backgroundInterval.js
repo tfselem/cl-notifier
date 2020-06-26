f041b6079a3eb8c727c9e7515a8cdd07e727eed5
Thomas Selem <tfselem@gmail.com>
2020-06-25 22:25:39 -0400

    Began writing document parser for Cl search pages

--- scripts/backgroundInterval.js
+++ scripts/backgroundInterval.js
@@ -1,33 +1,87 @@
+function ClPage(doc) {
+    this.doc = doc;
+    this.resultsRows = doc.querySelector("#sortable-results ul.rows");
 
-function getClSearch() {
-    const interval = 60000;
-    chrome.storage.sync.get("savedSearches", function(res) {
-        if (res && res.savedSearches) {
-            console.log(res.savedSearches);
+    /**
+     * Gets the newest post publish date on the provided
+     * 'doc' craigslist page.
+     */
+    this.getNewestUnparsedPostDate = function() {
+        let firstResult = this.resultsRows.firstElementChild,
+            firstResultTime = firstResult ? firstResult.getElementsByTagName("time") : null;
+        if (firstResult.tagName !== "LI" || !firstResultTime[0]) { /* No posts */
+            return null;
         }
+        return firstResultTime[0].getAttribute("datetime");
+    }
 
+    /**
+     * Parses the newest post date in the craigslist page.
+     */
+    this.getNewestPostDate = function() {
+        try {
+            return Date.ClParse(this.getNewestUnparsedPostDate());
+        } catch(e) {
+            return null;
+        }
+    }
+
+    /**
+     * Gets results prior to <h4>Few local results found...</h4>,
+     * or if no local results are found, returns an empty array
+     */
+    this.getLocalResults = function() {
+        let localResults = [];
+        for (let i = 0; i < this.resultsRows.children.length; i++) {
+            if (this.resultsRows.children[i].tagName === "H4") {
+                break;
+            }
+            localResults.push(this.resultsRows.children[i]);
+        }
+        return localResults;
+    }
+
+    /**
+     * Gets posts after specified date
+     */
+    this.getPostsAfter = function() {
+    }
+}
+
+function getClXHR(xhr, interval, isLastIndex) {
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            /* DO STUFF */
+            let page = new ClPage(xhr.response);
+            console.log(page.getNewestPostDate());
+            console.log(page.getLocalResults());
+            /* /DO STUFF */
+            if (isLastIndex) { 
+                setTimeout(updateClSearch, interval);
+            }
+        }
+    }
+}
+
+function updateClSearch() {
+    const interval = 10000;
+    chrome.storage.sync.get("savedSearches", function(res) {
         if (res.savedSearches.length === 0) {
-            setTimeout(getClSearch, interval);
+            setTimeout(updateClSearch, interval);
             console.log("No saved searches");
         }
 
-        for (var i = 0; i < res.savedSearches.length; ++i) {
+        for (var i = 0; i < res.savedSearches.length; i++) {
             let xhr = new XMLHttpRequest();
             xhr.open("GET", res.savedSearches[i].url, true);
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    /* DO STUFF */
-                    console.log(xhr);
-                    if (i === res.savedSearches.length) {
-                        setTimeout(getClSearch, interval);
-                    }
-                }
-            }
+            xhr.responseType = "document";
+            getClXHR(xhr, interval, i === res.savedSearches.length-1);
             xhr.send();
         }
+
     });
 }
 
-chrome.runtime.onInstalled.addListener(getClSearch);
-chrome.runtime.onStartup.addListener(getClSearch);
+chrome.runtime.onInstalled.addListener(updateClSearch);
+chrome.runtime.onStartup.addListener(updateClSearch);
 
--- scripts/tools/date-parser.js
+++ scripts/tools/date-parser.js
@@ -3,9 +3,14 @@
  *  post date
  */
 Date.ClParse = function(str) {
+    if (str === null || str === undefined) {
+        throw "null parameter";
+    }
+
     const dateRegexp = /^(\d{4})-(\d{2})-(\d{2})\ (\d{2}):(\d{2})/;
     let date = new Date(),
         match = str.match(dateRegexp);
+
     if (match.length === 6) {
         date.setYear(parseInt(match[1]));
         date.setMonth(parseInt(match[2]) - 1);
@@ -13,6 +18,8 @@ Date.ClParse = function(str) {
         date.setHours(parseInt(match[4]));
         date.setMinutes(parseInt(match[5]));
         date.setSeconds(0);
+    } else {
+        throw "invalid parameter";
     }
     return date;
 }
